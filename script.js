f10bae893c3284664b8c31e39e46d50a36d2a775
Drew <drewkretschmar@yahoo.com>
2023-03-23 10:33:41 -0400

    added feedback prompts to each criteria

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var passwordLength = 
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = "0123456789";
+var special = "!@#$%^&*()";
+var passwordLength = "";
 
 // Write password to the #password input
 function writePassword() {
@@ -15,14 +18,19 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function promptCriteria() {
-  alert("Please select which criteria to use when generating your new password.")
-  prompt("How long do you want your new passowrd to be? Please choose between 8-128 characters.");
-  confirm("Would you like to include lowercase characters?");
-  confirm("Would you like to include uppercase characters?");
-  confirm("Would you like to include numeric characters?");
-  confirm("Would you like to include special characters?");
-  alert("Generating Password...");
-  alert("Your new password is: ");
+function generatePassword() {
+  var passwordLength = prompt("Enter password length", "8-128");
+  var includeLowercase = confirm("Include lowercase characters?");
+  if (includeLowercase === true) alert("Including lowercase characters");
+  else alert("No lowercase characters will be included");
+  var includeUppercase = confirm("Include uppercase characters?");
+  if (includeUppercase === true) alert("Including uppercase characters");
+  else alert("No uppercase characters will be included");
+  var includeNumeric = confirm("Include numeric characters?");
+  if (includeNumeric === true) alert("Including numeric characters");
+  else alert("No numeric characters will be included");
+  var includeSpecial = confirm("Include special characters?");
+  if (includeSpecial === true) alert("Including special characters");
+  else alert("No special characters will be included");
   return;
-};
+}

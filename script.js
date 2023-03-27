1a441bfd640b0e6f78e4014417979c3a0f34d9c6
Drew <drewkretschmar@yahoo.com>
2023-03-27 15:21:48 -0400

    added logic to javascript

--- script.js
+++ script.js
@@ -6,8 +6,45 @@ var numeric = "0123456789";
 var special = "!@#$%^&*()";
 var passwordLength = "";
 
+function generatePassword() {
+  var userPassword = "";
+  var options = "";
+  console.log("start");
+  var passwordLength = prompt("Enter password length", "8-128");
+  if (passwordLength < 8 || passwordLength > 128);
+  var includeLowercase = confirm("Include lowercase characters?");
+  console.log(options);
+  if (includeLowercase === true) {
+    options = options.concat(lowercase);
+  };
+  console.log(options);
+  var includeUppercase = confirm("Include uppercase characters?");
+  if (includeUppercase === true) {
+    options = options.concat(uppercase);
+  }
+  console.log(options);
+  var includeNumeric = confirm("Include numeric characters?");
+  if (includeNumeric === true) {
+    options = options.concat(numeric);
+  }
+  console.log(options);
+  var includeSpecial = confirm("Include special characters?");
+  if (includeSpecial === true) {
+    options = options.concat(special);
+  }
+  console.log(options);
+
+for (let i = 0; i < passwordLength; i++) {
+  userPassword += options.charAt(Math.floor(Math.random() * passwordLength));
+  console.log(userPassword);
+}
+
+  return userPassword;
+}
+
 // Write password to the #password input
 function writePassword() {
+  console.log("hello");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -18,19 +55,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword() {
-  var passwordLength = prompt("Enter password length", "8-128");
-  var includeLowercase = confirm("Include lowercase characters?");
-  if (includeLowercase === true) alert("Including lowercase characters");
-  else alert("No lowercase characters will be included");
-  var includeUppercase = confirm("Include uppercase characters?");
-  if (includeUppercase === true) alert("Including uppercase characters");
-  else alert("No uppercase characters will be included");
-  var includeNumeric = confirm("Include numeric characters?");
-  if (includeNumeric === true) alert("Including numeric characters");
-  else alert("No numeric characters will be included");
-  var includeSpecial = confirm("Include special characters?");
-  if (includeSpecial === true) alert("Including special characters");
-  else alert("No special characters will be included");
-  return;
-}
